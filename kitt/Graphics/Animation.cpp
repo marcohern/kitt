474f7c475b5010bca8aafd9da0c578f242324eaf
Marco Hernandez <marcohern@gmail.com>
2017-08-08 14:43:11 -0500

    created animation class

---
+++ kitt/Graphics/Animation.cpp
@@ -0,0 +1,46 @@
+
+#include "Animation.hpp"
+
+namespace Graphics
+{
+	Animation::Animation(int qty, double duration)
+		: qty(qty), duration(duration), current(NULL), index(0), acc(0.0)
+	{
+		dpf = duration / qty;
+		sprites = new Sprite*[qty];
+	}
+
+	Animation::~Animation() {
+		delete sprites;
+	}
+
+	void Animation::setSprite(int i, Sprite *sprite) {
+		sprites[i] = sprite;
+		current = sprites[i];
+	}
+
+	void Animation::setCurrentSprite(int i) {
+		index = i;
+		current = sprites[i];
+	}
+
+	Sprite *Animation::getSprite(int i) {
+		return sprites[i];
+	}
+
+	void Animation::nextSprite() {
+		index++;
+		if (index >= qty) {
+			index = 0;
+		}
+		current = sprites[index];
+	}
+
+	void Animation::update(double interval) {
+		acc += interval;
+		if (acc > dpf) {
+			acc -= dpf;
+			nextSprite();
+		}
+	}
+}
---
+++ kitt/Graphics/Animation.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Sprite.hpp"
+
+namespace Graphics
+{
+	class Animation {
+	private:
+		Sprite **sprites;
+		Sprite *current;
+
+		int qty;
+		double duration;
+		double dpf;
+		double acc;
+		int index;
+
+	public:
+		Animation(int qty, double duration);
+
+		~Animation();
+
+		void setSprite(int i, Sprite *sprite);
+		void setCurrentSprite(int i);
+		void nextSprite();
+		Sprite *getSprite(int i);
+		Sprite *getCurrentSprite();
+		void update(double interval);
+	};
+}

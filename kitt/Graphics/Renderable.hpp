416bf6f5e23d1b25926a77c37358add3beff9a10
Marco Hernandez <marcohern@gmail.com>
2017-08-10 17:48:22 -0500

    renderable

--- kitt/Graphics/Renderable.hpp
+++ kitt/Graphics/Renderable.hpp
@@ -10,28 +10,32 @@
 #define Renderable_hpp
 
 #include <stdio.h>
-#include "../Behaviors/Areable.hpp"
+#include "../Behaviors/Transformable.hpp"
+#include "Animation.hpp"
 #include "Renderer.hpp"
 
 namespace Graphics {
     using namespace Core;
 	using namespace Behaviors;
     
-    class Renderable : public Areable {
-    protected:
-        Renderer *renderer;
+    class Renderable : public Transformable {
         
     public:
         Renderable();
         Renderable(const Vector2D &location);
-        Renderable(const Vector2D &location, const Vector2D &size,const Vector2D &pivot);
-        Renderable(Renderer *renderer, Trigonometry *trigo);
-        Renderable(const Vector2D &location,Renderer *renderer, Trigonometry *trigo);
-        Renderable(const Vector2D &location, const Vector2D &size,const Vector2D &pivot,Renderer *renderer, Trigonometry *trigo);
+		Renderable(const Vector2D &location, const Vector2D &size);
+        Renderable(const Vector2D &location, const Vector2D &size,const Vector2D &areaPivot);
+		Renderable(const Vector2D &location, const Vector2D &size, const Vector2D &areaPivot, const Vector2D &rotatePivot);
+
+        Renderable(Trigonometry *trigo);
+        Renderable(const Vector2D &location, Trigonometry *trigo);
+		Renderable(const Vector2D &location, const Vector2D &size, Trigonometry *trigo);
+        Renderable(const Vector2D &location, const Vector2D &size,const Vector2D &areaPivot, Trigonometry *trigo);
+		Renderable(const Vector2D &location, const Vector2D &size, const Vector2D &areaPivot, const Vector2D &rotatePivot, Trigonometry *trigo);
         
         virtual ~Renderable();
         
-        virtual void render() = 0;
+        virtual void render(Renderer *render) = 0;
     };
 }
 #endif /* Renderable_hpp */

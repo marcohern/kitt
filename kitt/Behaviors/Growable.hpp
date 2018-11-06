0048686232532fbe59667107e8cc531c7c173714
Marco Hernandez <marcohern@gmail.com>
2018-11-05 23:19:41 -0500

    added more documentation

--- kitt/Behaviors/Growable.hpp
+++ kitt/Behaviors/Growable.hpp
@@ -14,26 +14,78 @@
 #include "../Core/Trigonometry.hpp"
 
 namespace Behaviors{
+
+	/**
+	* Allows ability to grow or shrink (chnage scale)
+	*/
     class Growable : public Scalable {
     protected:
+		//Rage of growth in both X and Y
         Vector2D growthRate;
         
     public:
+		/**
+		* Constructor
+		*/
         Growable();
         
+		/**
+		* Destructor
+		*/
         virtual ~Growable();
         
+		/**
+		* Set Growth Rate
+		* @param g Scalar Growth rate
+		*/
         void setGrowthRate(double g);
+
+		/**
+		* Set Growth Rate
+		* @param g Vector Growth rate
+		*/
         void setGrowthRate(const Vector2D &g);
         
+		/**
+		* Get Growth Rate
+		* @returns Vector Growth Rate
+		*/
         Vector2D getGrowthRate() const;
+
+		/**
+		* Get Growth Rate X
+		* @returns Growth Rate X
+		*/
         double getGrowthRateX() const;
+
+		/**
+		* Get Growth Rate Y
+		* @returns Growth Rate Y
+		*/
         double getGrowthRateY() const;
         
+		/**
+		* Add to Growth Rate
+		* @param ds Growth Displacement
+		*/
         void addGrowth(double ds);
+
+		/**
+		* Add to Growth Rate
+		* @param d Growth Displacement
+		*/
         void addGrowth(const Vector2D &d);
+
+		/**
+		* Multiply Growth
+		* @param ms Growth Factor 
+		*/
         void multiplyGrowth(double ms);
         
+		/**
+		* Update Scale
+		* @param interval Time Interval
+		*/
         void updateScale(double interval);
     };
 }
--- kitt/Behaviors/Rotateable.hpp
+++ kitt/Behaviors/Rotateable.hpp
@@ -12,23 +12,82 @@
 #include "Angleable.hpp"
 
 namespace Behaviors {
+
+	/**
+	* Allows an object to rotate
+	*/
     class Rotateable : public Angleable {
     protected:
         double rotationRate;
         
     public:
+
+		/**
+		* Constructor
+		*/
         Rotateable();
+
+		/**
+		* Constructor
+		* @param radians Angle in Radians
+		*/
         Rotateable(double radians);
+
+		/**
+		* Constructor
+		* @param px Rotation Pivot X
+		* @param py Rotation Pivot Y
+		*/
         Rotateable(double px, double py);
+
+		/**
+		* Constructor
+		* @param pivot Rotation Pivot
+		*/
         Rotateable(const Vector2D &pivot);
+
+		/**
+		* Constructor
+		* @param px Rotation Pivot X
+		* @param py Rotation Pivot Y
+		* @param radians Angle in Radians
+		*/
         Rotateable(double px, double py, double radians);
+
+		/**
+		* Constructor
+		* @param pivot Rotation Pivot
+		* @param radians Angle in Radians
+		*/
         Rotateable(const Vector2D &pivot, double radians);
         
-        ~Rotateable();
+		/**
+		* Destructor
+		*/
+        virtual ~Rotateable();
         
+		/**
+		* Set the Rotation Rate
+		* @param rate Rotation Rate
+		*/
         void setRotationRate(double rate);
+
+		/**
+		* Add to the Rotation Rate
+		* @param rate value to add
+		*/
         void addRotationRate(double rate);
+
+		/**
+		* Get Rotation Rate
+		* @returns Rotation Rate
+		*/
         double getRotationRate() const;
+
+		/**
+		* Update angle given rotationrate
+		* @returns Time interval
+		*/
         void updateAngle(double interval);
     };
 }
